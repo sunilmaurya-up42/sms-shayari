74dcd08cb21e39694392a06458b98bcc7f9ce413
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-06-20 21:42:43 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,1 +1,65 @@
+const express = require("express");
 
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+
+const adminController = require(
+
+"../controllers/adminController"
+
+);
+
+
+
+// Login Page
+
+router.get(
+
+"/login",
+
+adminController.loginPage
+
+);
+
+
+
+// Login
+
+router.post(
+
+"/login",
+
+adminController.login
+
+);
+
+
+
+// Dashboard
+
+router.get(
+
+"/dashboard",
+
+auth,
+
+adminController.dashboard
+
+);
+
+
+
+// Logout
+
+router.get(
+
+"/logout",
+
+adminController.logout
+
+);
+
+
+
+module.exports = router;
