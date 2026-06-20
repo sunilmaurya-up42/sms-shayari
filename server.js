a6e5aeb6ed0c06cbf3df887c3daa62e9680b1fe6
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-06-20 21:39:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,99 @@
+require("dotenv").config();
 
+
+
+const express=require("express");
+
+
+const session=require("express-session");
+
+
+const MongoStore=require("connect-mongo");
+
+
+const connectDB=require("./config/db");
+
+
+
+const app=express();
+
+
+
+connectDB();
+
+
+
+app.use(express.urlencoded({extended:true}));
+
+
+
+app.use(express.json());
+
+
+
+app.use(express.static("public"));
+
+
+
+app.set("view engine","ejs");
+
+
+
+app.use(session({
+
+
+
+secret:process.env.SESSION_SECRET,
+
+
+
+resave:false,
+
+
+
+saveUninitialized:false,
+
+
+
+store:MongoStore.create({
+
+
+
+mongoUrl:process.env.MONGO_URI
+
+
+
+})
+
+
+
+}));
+
+
+
+
+app.get("/",(req,res)=>{
+
+
+
+res.send("SMS Shayari Running");
+
+
+
+});
+
+
+
+const PORT=process.env.PORT||3000;
+
+
+
+app.listen(PORT,()=>{
+
+
+
+console.log(`Server Running on ${PORT}`);
+
+
+
+});
