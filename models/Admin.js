53e6acdfe11a54c95850b5c9c41352db24c40b8f
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-06-20 21:37:06 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,1 +1,39 @@
+const mongoose=require("mongoose");
 
+
+
+const AdminSchema=new mongoose.Schema({
+
+
+
+email:{
+
+
+type:String,
+
+required:true,
+
+unique:true
+
+
+},
+
+
+
+password:{
+
+
+type:String,
+
+required:true
+
+
+}
+
+
+
+});
+
+
+
+module.exports=mongoose.model("Admin",AdminSchema);
