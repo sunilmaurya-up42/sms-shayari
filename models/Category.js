69ea182e9dbb6a6f357c92d36b1477b255a103a3
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-06-20 21:37:29 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,1 +1,39 @@
+const mongoose=require("mongoose");
 
+
+
+const CategorySchema=new mongoose.Schema({
+
+
+
+name:{
+
+
+type:String,
+
+required:true
+
+
+},
+
+
+
+slug:{
+
+
+type:String,
+
+required:true,
+
+unique:true
+
+
+}
+
+
+
+});
+
+
+
+module.exports=mongoose.model("Category",CategorySchema);
