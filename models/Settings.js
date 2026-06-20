e4fa6a2cf8ec047772c3091e24f0bbf4459b9c0b
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-06-20 21:38:34 +0530

    Update Settings.js

--- models/Settings.js
+++ models/Settings.js
@@ -1,1 +1,32 @@
+const mongoose=require("mongoose");
 
+
+
+const SettingsSchema=new mongoose.Schema({
+
+
+
+siteName:String,
+
+
+logo:String,
+
+
+phone:String,
+
+
+whatsapp:String,
+
+
+email:String,
+
+
+analytics:String
+
+
+
+});
+
+
+
+module.exports=mongoose.model("Settings",SettingsSchema);
