98c6ddb235b89fec89219140ad32af39b6c29d48
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-06-21 11:25:51 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,8 @@ module.exports = (req, res, next) => {
 
     if (!req.session.admin) {
 
+        req.session.message = "Please login first";
+
         return res.redirect("/admin/login");
 
     }
