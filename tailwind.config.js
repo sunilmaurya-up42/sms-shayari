ba4d2ae2cd518c9f8ab7a8204852bec640cea762
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-06-22 11:41:46 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.ejs",
+    "./public/js/**/*.js"
+  ],
 
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+}
