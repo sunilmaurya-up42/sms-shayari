b339031be02448a02fb6957c53602a9cc13a0675
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-06-23 00:18:22 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,10 @@
 module.exports = (req, res, next) => {
 
-    console.log("AUTH SESSION :", req.session);
-
-    if (req.session && req.session.admin) {
-
+    if (
+        req.session &&
+        req.session.admin === true
+    ) {
         return next();
-
     }
 
     return res.redirect("/admin/login");
