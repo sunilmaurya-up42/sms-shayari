61534bce98a2757be684073daa31a21fdd43ea7d
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-06-25 23:03:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -170,31 +170,74 @@ adminRoutes
 
 );
 
+
 // ===========================
 // Sitemap.xml
 // ===========================
 
 app.get("/sitemap.xml", async (req, res) => {
 
-res.header("Content-Type", "application/xml");
+try{
+
+const shayari = await Shayari.find({
+published:true
+});
+
+let urls = "";
+
+shayari.forEach(item=>{
+
+urls += `
+
+<url>
+
+<loc>
+
+https://sms-shayari.onrender.com/shayari/${item.slug}
+
+</loc>
+
+<priority>0.8</priority>
+
+</url>
+
+`;
+
+});
 
 const xml = `<?xml version="1.0" encoding="UTF-8"?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
 <url>
-<loc>https://sms-shayari.onrender.com/</loc>
+
+<loc>
+
+https://sms-shayari.onrender.com/
+
+</loc>
+
 <priority>1.0</priority>
+
 </url>
 
-<url>
-<loc>https://sms-shayari.onrender.com/contact</loc>
-<priority>0.8</priority>
-</url>
+${urls}
 
 </urlset>`;
 
+res.header("Content-Type","application/xml");
+
 res.send(xml);
+
+}
+
+catch(err){
+
+console.log(err);
+
+res.status(500).send("Sitemap Error");
+
+}
 
 });
 
