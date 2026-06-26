0451fc542c50bbb2cc558d3624e3e21a91151ee4
Sunil Maurya <sunilmauryasurila@gmail.com>
2026-06-26 22:35:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 
 
 
+
+
 // ===========================
 // MongoDB Connection
 // ===========================
@@ -44,20 +46,9 @@ console.log(err);
 // ===========================
 // Body Parser
 // ===========================
-
-app.use(
-
-express.urlencoded({
-
-extended: true
-
-})
-
-);
-
-app.use(
-
-express.json()
+app.use(compression());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()
 
 );
 
